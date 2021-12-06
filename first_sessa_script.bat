c1449d9684c3e7d9e107225c69349cce0b28a8c4
pipclark <94843495+pipclark@users.noreply.github.com>
2021-12-06 18:29:24 +0100

    Add files via upload

---
+++ first_sessa_script.bat
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 19 12:09:44 2020
+
+@author: Pip C J Clark
+"""
+
+ECHO ON
+REM A batch script to execute a Python script
+SET PATH=C:\ProgramData\Anaconda3
+python SESSAautomaterscript1_generatingsessionfiles.py
+PAUSE
+
---
+++ second_sessa_script.bat
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 19 12:28:21 2020
+
+@author: Pip C J Clark
+
+"""
+
+ECHO ON
+REM A batch script to execute a Python script
+SET PATH=C:\ProgramData\Anaconda3
+python SESSAautomaterscript2_runningthesessionfiles.py
+PAUSE
+
